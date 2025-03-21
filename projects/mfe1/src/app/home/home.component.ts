472101d1f0a5eae9571eb360ed37b8188db2a8bd
kaleb22 <kaleb22@hotmail.com>
2025-03-21 09:24:58 -0300

    feat(message-service): using service to send messages between shell and mfe1

--- projects/mfe1/src/app/home/home.component.ts
+++ projects/mfe1/src/app/home/home.component.ts
@@ -1,22 +1,40 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
 
-import { LogService } from '@shared';
+import { LogService, MessageService } from '@shared';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   imports: [
-    RouterModule
+    RouterModule,
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   private logService = inject(LogService);
+  private messageService = inject(MessageService);
+  private sub = new Subscription();
+
+  message;
 
   ngOnInit(): void {
     this.logService.log('MFE1 initialized successfully');
+    this.sub.add(
+      this.messageService.retrieveData()
+        .subscribe( (e: CustomEvent) => this.message = e.detail)
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  sendMessage() {
+    const message = 'Hello from MFE1 :D'
+    this.messageService.sendData(message);
   }
 
 }
--- projects/shared-lib/src/public-api.ts
+++ projects/shared-lib/src/public-api.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './lib/log.service';
+export * from './lib/message.service'
