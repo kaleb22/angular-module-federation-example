adfe652bb4025ca272512e812dfbba25d2b13243
kaleb22 <kaleb22@hotmail.com>
2025-05-15 14:29:05 -0300

    chore: removing shared-lib and mfe2 code

--- projects/mfe1/src/app/home/home.component.ts
+++ projects/mfe1/src/app/home/home.component.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Subscription } from 'rxjs';
-
-import { LogService, MessageService } from '@shared';
 
 @Component({
   selector: 'app-home',
@@ -13,29 +10,5 @@ import { LogService, MessageService } from '@shared';
     RouterModule,
   ]
 })
-export class HomeComponent implements OnInit, OnDestroy {
-
-  private logService = inject(LogService);
-  private messageService = inject(MessageService);
-  private sub = new Subscription();
-
-  message;
-
-  ngOnInit(): void {
-    this.logService.log('MFE1 initialized successfully');
-    this.sub.add(
-      this.messageService.retrieveData()
-        .subscribe( (e: CustomEvent) => this.message = e.detail)
-    )
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
-
-  sendMessage() {
-    const message = 'Hello from MFE1 :D'
-    this.messageService.sendData(message);
-  }
-
+export class HomeComponent {
 }
--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Subscription } from 'rxjs';
-
-import { LogService, MessageService } from '@shared';
 
 @Component({
   standalone: true,
@@ -13,33 +10,7 @@ import { LogService, MessageService } from '@shared';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'app-shell';
-
-  private logService = inject(LogService)
-  private messageService = inject(MessageService);
-  private sub = new Subscription();
-
-  message;
-
-  ngOnInit(): void {
-    this.logService.log('testing logging library');
-   this.sub.add( this.messageService.retrieveData()
-      .subscribe( (e: CustomEvent) => this.message = e.detail)
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
-
-  getMessageFromMFEs() {
-    this.message = this.messageService.retrieveData();
-  }
-
-  sendMessage() {
-    const msg = 'Hello from Shell :D';
-    this.messageService.sendData(msg);
-  }
 }
 
