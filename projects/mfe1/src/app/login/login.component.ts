e90a98e9eb6f592243e5efb3bcef8e8307715495
kaleb22 <kaleb22@hotmail.com>
2025-05-20 10:56:00 -0300

    feat(login-component): creating reactive form and applying validation on fields

--- projects/mfe1/src/app/login/login.component.ts
+++ projects/mfe1/src/app/login/login.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  private formBuilder = inject(FormBuilder);
+
+  loginForm: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8),
+        Validators.pattern(/(?=.*\d)(?=.*[A-Z]).*/)
+        ])
+    });
+  }
+
+  checkError(controlName: string) {
+    return {
+      'has-error': this.verifyValidTouched(controlName)
+    }
+  }
+
+  verifyValidTouched(controlName: string) {
+    return !this.loginForm.get(controlName).valid && this.loginForm.get(controlName).touched;
+  }
+
+  login() {
+    console.log(this.loginForm.valid);
+  }
 }
